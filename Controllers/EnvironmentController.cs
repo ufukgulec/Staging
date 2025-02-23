9c7769d4acfbd54fe50d998feb8034e5555fc8b9
ufukgulec <orhanufukgulec@gmail.com>
2025-02-23 14:56:30 +0300

    Proje aktarımı

---
+++ Controllers/EnvironmentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Staging.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnvironmentController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfiguration _config;
+
+        public EnvironmentController(IWebHostEnvironment env, IConfiguration config)
+        {
+            _env = env;
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult GetEnvironment()
+        {
+            return Ok(new
+            {
+                Environment = _env.EnvironmentName,
+                ConnectionString = _config.GetConnectionString("DefaultConnection")
+            });
+        }
+    }
+
+}
